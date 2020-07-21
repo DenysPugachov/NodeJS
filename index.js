c46a518d134376a1302d442bf3961835f6b4c0b3
Denys <pugachovdenys@gmail.com>
2020-07-21 22:54:15 +0300

    finall commit

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const wetherRequest = require("./requests/wether.request");
+const weatherRequest = require("./requests/weather.request");
 
 const app = express();
 
-//0da2083414719a1e8dd793bdf9314697
-
 app.set("view engine", "ejs");
-app.use(express.static("public")); //where is static file?
-app.use(bodyParser.urlencoded({ extended: true })); //use bodyParser for parse data;
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", { weather: null, error: null });
 });
 
-app.post("/", (req, res) => {
-    const { city } = req.body; //destruction
-    wetherRequest(city);
-    console.log(city);
-    res.render("index"); //what page what to return?
+app.post("/", async (req, res) => {
+    const { city } = req.body;
+
+    const { weather, error } = await weatherRequest(city);
+    res.render("index", { weather, error });
 });
 
 app.listen(3000, () => {
--- requests/wether.request.js
+++
@@ -1,9 +0,0 @@
-// const rp = require("request-promise");
-
-module.exports = async function(city = " ") {
-    if (!city) {
-        throw new Error(`City in empty`);
-    }
-
-    console.log("City", city);
-};
